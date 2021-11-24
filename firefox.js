cbd741469c4660bf0a2cb19ea2ef57892c080a3e
Lisenaaaa <69115669+Lisenaaaa@users.noreply.github.com>
2021-11-24 09:50:33 -0800

    finish firefox - youtube

--- firefox.js
+++ firefox.js
@@ -4,16 +4,47 @@ const bus = require('dbus-next').sessionBus()
 const args = process.argv
 args.shift()
 args.shift()
+
+async function getFirefoxName() {
+	let obj = await bus.getProxyObject('org.freedesktop.DBus', '/org/freedesktop/DBus')
+	let iface = obj.getInterface('org.freedesktop.DBus')
+	let names = await iface.ListNames()
+	let result = names.filter((n) => n.startsWith('org.mpris.MediaPlayer2.firefox'))
+
+    return result[0]
+}
+
 ;(async () => {
 	try {
-		const firefox = await bus.getProxyObject('org.mpris.MediaPlayer2.firefox', '/org/mpris/MediaPlayer2/firefox')
+		const firefox = await bus.getProxyObject(await getFirefoxName(), '/org/mpris/MediaPlayer2')
+		const player = firefox.getInterface('org.mpris.MediaPlayer2.Player')
+		const properties = firefox.getInterface('org.freedesktop.DBus.Properties')
 
-        console.log(firefox)
+		const metadata = await properties.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
+
+		const youtubeTitle = metadata.value['xesam:title'].value
+
+		switch (args[0] ? args[0].toLowerCase() : undefined) {
+			case 'playpause':
+				await player.PlayPause()
+				break
+			case 'getcurrent':
+				console.log(youtubeTitle)
+				break
+			case 'previous':
+				await player.Previous()
+				break
+			case 'next':
+				await player.Next()
+				break
+
+			case undefined:
+				console.log(firefox.name.split('.')[firefox.name.split('.').length - 1])
+		}
 		process.exit()
 	} catch (err) {
-		// if (err.message === 'The name org.mpris.MediaPlayer2.firefox was not provided by any .service files') console.log('')
-		// else (console.error(err))
-        console.error(err)
+		if (err.message === 'Invalid bus name: undefined') console.log('')
+		else (console.error(err))
         process.exit()
 	}
 })()
